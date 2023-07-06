10ced0ad7fb358397dda038ee6b25e56026c98f7
Anremithrl <austingmath@skiff.com>
2023-07-06 08:05:07 -0700

    string processing and tuple computation

--- expr.py
+++ expr.py
@@ -6,19 +6,21 @@ expression = tuple[str, tuple[Any, ...]]
 
 equality = tuple[expression, expression]
 
-@dataclass(eq=False, hash=False, frozen=True)
-class expression_wrapper:
-    expr:expression
-
-    def __str__(self):
-        return stringify(expr)
-
-    __repr__ = __str__
-
 class math_namespace(dict): # I have some plans for this, not implimented yet.
     pass
 
-@dataclass(frozen=True)
+@dataclass(eq=False, unsafe_hash=True, frozen=True)
+class expression_wrapper:
+    expr:expression
+    namespace: math_namespace
+
+    def __str__(self):
+        return stringify(self.expr, self.namespace)
+
+    __repr__ = __str__
+
+
+@dataclass(frozen=True, slots=True)
 class math_symbol:
     symbol: str
     num_args:int
@@ -32,15 +34,45 @@ class math_symbol:
     def __post_init__(self):
         self.namespace[self.symbol]=self
 
-def stringify(expr):
-    return NotImplemented
-    #return string version according to strepr
+    def __str__(self):
+        return strepr
+
+def stringify(expr, namespace):
+    if not isinstance(expr, tuple):
+        return str(expr)
+    symbol = namespace[expr[0]]
+    temp = symbol.strepr
+    return _string_inc(temp, expr, lambda n: stringify(n, namespace))
+
+def latexify(expr, namespace):
+    if not isinstance(expr, tuple):
+        return str(expr)
+    symbol = namespace[expr[0]]
+    temp = symbol.latex
+    return _string_inc(temp, expr, lambda n: latexify(n, namespace))
+
+def _string_inc(run, expr, function):
+    for num, arg in enumerate(expr[1:]):
+        run = run.replace(f'${num}$', function(arg))
+    return run
+#return string version according to strepr
 
 def apply(equa, expr):
     return NotImplemented
 #return an application of a rule to an expression
 
-def compute(expr):
-    return NotImplemented
+def compute(expr, namespace):
+    if not isinstance(expr, tuple):
+        return expr
+    symbol = namespace[expr[0]]
+    return symbol.compute(*(compute(a, namespace) for a in expr[1:]))
 
+if __name__ == '__main__':
+    mns = math_namespace()
+    Plus = math_symbol('+', 2, lambda a,b:a+b, mns, '$0$+$1$', '{$0$}+{$1$}', (), ())
+    # Test = expression_wrapper(('+', 1, ('+', 1, 2)), mns)
+    # str(Test)
+    # stringify(Test.expr, mns)
+    # latexify(Test.expr, mns)
+    # compute(Test.expr, mns)
 
