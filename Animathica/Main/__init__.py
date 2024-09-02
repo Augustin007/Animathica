0d141c0a4d1998edd037bce3be036b260b2af400
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2024-09-02 12:48:57 -0700

    Flow of the game notes

---
+++ Animathica/Main/__init__.py
@@ -0,0 +1,11 @@
+# Flow of the game.
+def main():
+    Back = True
+    while Back:
+        # Technically should be all async and stuff and display, but this is more for keeping it straight in my head.
+        # Choice = DisplayMenu.mainloop() (modes: Tutorial, Level Select, Constructive, Omnipotent, + settings? And exit). This function displays the menu and returns the correct mode.
+        # if Choice == Exit: break
+        # Current = Choice.mainloop() # Choose specific level.
+        # Back = Current.mainloop()
+        # Do we make this a while loop?
+        pass
