54ce9bc2ffff24d631ec038404204843985dcc55
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2024-09-02 12:32:49 -0700

    Fixed bug where (x) was not equal to x

--- Animathica/Calculator/rudimentary_simplifier.py
+++ Animathica/Calculator/rudimentary_simplifier.py
@@ -3,9 +3,9 @@ from numbers import Number
 from fractions import Fraction
 from operator import add, mul, call
 from abc import abstractmethod
-from ..logger import log, log_function, log_class, logging, log_skip
+from ..logger import log, log_function, log_class, logging, log_skip, log_flags
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class expression:
     __radd__ = __add__ = lambda self, other: addition(self, other)
     __sub__ = lambda self, other: addition(self, multiplication(other, -1))
@@ -15,12 +15,13 @@ class expression:
     __rtrudediv__ = lambda self, other: multiplication(exponentiation(self, -1), other)
     __pow__ = lambda self, other: exponentiation(self, other)
     __rpow__ = lambda self, other: exponentiation(other, self)
+    __neg__ = lambda self: multiplication(self, -1)
     def simplify(self):
         return self
     def on_simplify(self):
         return self
 
-@log_function(logging.INFO)
+@log_function(logging.DEBUG)
 def safe(*element):
     if len(element) != 1:
         return map(safe, element)
@@ -33,7 +34,7 @@ def safe(*element):
         return variable(a)
     return a
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class binary_operation(expression):
     a: expression
     b: expression
@@ -53,14 +54,17 @@ class binary_operation(expression):
         return f'({self.a}{self.infix}{self.b})'
 
     def simplify(self)->expression:
+        sa = self.a.simplify()
+        sb = self.b.simplify()
         if hasattr(self, 'identity'):
-            if self.a == self.identity:
-                return self.b
-            if self.b == self.identity:
-                return self.a
-        if isinstance(self.a, number) and isinstance(self.b, number):
-            return number(self.compute(self.a.value, self.b.value))
-        return self.on_simplify()
+            if sa.__simplified_eq__(self.identity):
+                return sb
+            if sb.__simplified_eq__(self.identity):
+                return sa
+        if isinstance(sa, number) and isinstance(sb, number):
+            return number(self.compute(sa.value, sb.value))
+        return type(self)(sa, sb).on_simplify()
+        #return self.on_simplify()
 
     def on_simplify(self)->expression:
         return self
@@ -68,13 +72,14 @@ class binary_operation(expression):
     def __eq__(self, other):
         sself = self.simplify()
         sother = other.simplify()
-        if type(sself) == type(self):
-            if type(sother) == type(sself):
-                return sself.a == sother.a and sself.b == sother.b
-            return False
-        return sself == sother
+        return sself.__simplified_eq__(sother)
 
-@log_class(logging.INFO)
+    def __simplified_eq__(self, sother):
+        if isinstance(sother, type(self)):
+            return self.a == sother.a and self.b == sother.b
+        return False
+
+@log_class(logging.DEBUG)
 class number(expression):
     def __init__(self, value):
         if int(value) == value:
@@ -88,11 +93,14 @@ class number(expression):
 
     def __eq__(self, other):
         sother = other.simplify()
+        return self.__simplified_eq__(sother)
+
+    def __simplified_eq__(self, sother):
         if type(sother) is number:
             return self.value == sother.value
         return False
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class variable(expression):
     name: str
     constant: bool
@@ -113,55 +121,86 @@ class variable(expression):
 
     def __eq__(self, other):
         sother = other.simplify()
+        return self.__simplified_eq__(sother)
+    
+    def __simplified_eq__(self, sother):
         if type(sother) is variable:
             return self.name == sother.name
         return False
 
-@log_function(logging.INFO)
-def flatten(this, over, that):
+@log_function(logging.DEBUG)
+def flatten(this, over, that, pre_simplified=False):
     '''
-    flattens instances of inner over instances of the outer
+    flattens an expression like (1+(1+1)) into an element of type that, flattening out over 'over' (+) to get (1+1+1)
     '''
     if isinstance(this, over):
         return (*flatten(this.a, over, that), *flatten(this.b, over, that))
     if isinstance(this, that):
+        if pre_simplified:
+            return this.items
         return (*this.simplify().items,)
+    if pre_simplified:
+        return (this,)
     return (this.simplify(),)
 variables: dict[str, variable] = {}
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class semiring(expression):
     inner: Any
     outer: Any
     items: tuple[expression, ...]
-    def __init__(self, *items):
+    def __init__(self, *items, pre_simplified=False):
         #for item in items:
         #    assert isinstance(item, self.inner)
         self.items = items
+        self.pre_simplified = pre_simplified
         # self.items = flatten(item, self.inner)
+        self.on_init()
+
+    def on_init(self):
+        return self
 
     def simplify(self)->expression:
-        if len(self.items) == 1:
-            items = list(map(self.split, flatten(self.items[0], self.inner, type(self))))
-        else: 
-            items = list(map(self.split, self.items))
+        items:list = []
+        for item in self.items:
+            if self.pre_simplified:
+                items.extend(map(self.split, flatten(item, self.inner, type(self), True)))
+                continue
+            items.extend(map(self.split, flatten(item.simplify(), self.inner, type(self))))
+        log(logging.INFO, f'Item count for {self.__class__}: {items}')
         constant = self.inner.identity
         new: list = []
         while items:
+            log(logging.INFO, f'While Loop, current item count {items}')
             numb, expression = items.pop()
+            log(logging.INFO, f'Current expression {expression}')
             if isinstance(expression, number):
                 constant = self.inner.compute(constant, expression)
                 continue
             index = 0
             for num, expr in items.copy():
+                log(logging.INFO, f'For loop, current item count {items}')
+                log(logging.INFO, f'Matching {expression} against {expr}')
+                log(logging.INFO, f'Matching {type(expression)} with {type(expr)}')
+                log(logging.INFO, f'{expr==expression}')
                 if expr == expression:
+                    log(logging.INFO, f'Matched {expression} with {expr}')
                     items.pop(index)
                     numb += num
+                    log(logging.INFO, f'Total = {numb}')
                     continue
                 index += 1
-            new.append(self.outer(expression, numb.simplify()).simplify())
-        if constant!= self.inner.identity:
+            log(logging.INFO, f'{expression}, {numb}')
+            numb = numb.simplify()
+            #if hasattr(self.inner, 'identity') and self.inner.identity.__simplified_eq__(numb):
+            #    continue
+            new.append(self.outer(expression, numb).simplify())
+        if constant != self.inner.identity:
             new.append(constant.simplify())
+        if len(new) == 0:
+            return self.inner.identity
+        if len(new) == 1:
+            return new[0]
         return self.__class__(*new)
 
     @abstractmethod
@@ -171,14 +210,16 @@ class semiring(expression):
     def __eq__(self, other):
         a = self.simplify()
         b = other.simplify()
-        if type(a) == type(self):
-            if type(other) == type(a):
-                for item in self.items:
-                    if item not in other.items:
-                        return False
-                return True
+        return a.__simplified_eq__(b)
+
+    def __simplified_eq__(self, other):
+        if type(other) != type(self):
             return False
-        return a == b
+        for item in self.items:
+            if item not in other.items:
+                return False
+            return True
+        return False
 
     def __repr__(self):
         return self.inner.infix.join(map(repr, self.items))
@@ -188,35 +229,38 @@ class addition(binary_operation):
     infix = '+'
     identity = number(0)
 
-    @log_function(logging.INFO)
+    @log_function(logging.DEBUG)
     def on_simplify(self):
-        return add_mul(self).simplify()
+        return add_mul(self.a, self.b, pre_simplified=True).simplify()
 
 class multiplication(binary_operation):
     compute = mul
     infix = '*'
     identity = number(1)
 
-    @log_function(logging.INFO)
+    @log_function(logging.DEBUG)
     def on_simplify(self):
-        return mul_pow(self).simplify()
+        if addition.identity in (self.a, self.b):
+            return addition.identity
+        return mul_pow(self.a, self.b, pre_simplified=True).simplify()
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class exponentiation(binary_operation):
     compute = pow
     infix = '**'
     def on_simplify(self):
         if self.b == number(1):
             return self.a
+        return self
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class nest(binary_operation):
     compute = call
     infix = ''
     def on_init(self):
         assert isinstance(self.a, function)
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class function:
     def __init__(self, func, safe_func):
         self.func = func
@@ -225,7 +269,7 @@ class function:
     def __repr__(self):
         return self.func.__name__
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class add_mul(semiring):
     inner = addition
     outer = multiplication
@@ -235,7 +279,7 @@ class add_mul(semiring):
                 return item.items[-1], mul_pow(*item.items[:-1])
         return number(1), item
 
-@log_class(logging.INFO)
+@log_class(logging.DEBUG)
 class mul_pow(semiring):
     inner = multiplication
     outer = exponentiation
@@ -245,3 +289,4 @@ class mul_pow(semiring):
             return item.b, item.a
         return number(1), item
 
+#log_flags['simplify'] = logging.INFO
